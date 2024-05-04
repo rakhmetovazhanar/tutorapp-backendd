dc818a69cc0f52250789bd43ba08c7ca2951205a
rakhmetovazhanar <200103526@stu.sdu.edu.kz>
2024-05-05 01:21:54 +0500

    routing 3

--- tutorapp/routing.py
+++ tutorapp/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path(r'^ws/conference/(?P<room_name>[^/]+)/$', consumers.ConferenceConsumer.as_asgi()),
+    path('ws/conference/<str:room_name>/', consumers.ConferenceConsumer.as_asgi()),
 ]
